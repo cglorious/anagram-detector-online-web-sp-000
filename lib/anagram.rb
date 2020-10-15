258552ec68308025aab8e36d3157d50446c3c005
Catherine Gloria <catherinegloria@gmail.com>
2020-10-15 03:42:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,6 +13,7 @@ attr_accessor :match
     array.each do |e|
       if e.split("").sort == @word.split("").sort
         anagram_match >> e
+        anagram_match
       #return array index
     end
   end
